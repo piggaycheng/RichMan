1f53720e0a282b638f8dd64d00b95416d7c0edf1
piggaycheng <piggaycheng123@gmail.com>
2018-09-30 17:01:10 +0800

    Add img on every cell

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    for(var i=0; i<10; i++){
+    for(var i=0; i<10; i++){                //畫四邊border
 
         $("#gameMap").append("<tr id = 'row" + i + "'></tr>");
 
@@ -12,16 +12,46 @@ $(document).ready(function(){
             }
         }
     }
+
+    mapArray.sort(function(a, b){               //用data-index屬性去排序這個一維陣列
+        var indexA = a.data("index");
+        var indexB = b.data("index");
+        if(indexA < indexB){
+            return -1;
+        }
+        if(indexA > indexB){
+            return 1;
+        }
+        return 0;
+    });
+
+    for(var i=0; i<mapArray.length; i++){
+        if(i == 0){
+            mapArray[i].append("<div>→</div><div class='headIcon headIcon1'></div><div class='headIcon headIcon2'></div>");
+        } else{
+            mapArray[i].append("<div class='levelArea'></div><div class='headIcon headIcon1'></div><div class='headIcon headIcon2'></div>");
+        }
+    }
+
+    $(".levelArea").append("<img><img><img>");
+    $(".headIcon1").append("<img class='c1'><img class='c2'>");
+    $(".headIcon2").append("<img class='c3'><img class='c4'>");
 });
 
-function toOneDimension(x, y){                  //轉為一維
+var mapArray = [];
+
+function toOneDimension(x, y){                  //轉為一維，把有用的cell都丟進一個一維陣列
     if(y == 0){
         $("#cell" + x + "-" + y).data("index", x);
+        mapArray.push($("#cell" + x + "-" + y));
     } else if(x == 9){
         $("#cell" + x + "-" + y).data("index", 9 + y);
+        mapArray.push($("#cell" + x + "-" + y));
     } else if(y == 9){
         $("#cell" + x + "-" + y).data("index", 27 - x);
+        mapArray.push($("#cell" + x + "-" + y));
     } else if(x == 0){
         $("#cell" + x + "-" + y).data("index", 36 - y);
+        mapArray.push($("#cell" + x + "-" + y));
     }
 }
